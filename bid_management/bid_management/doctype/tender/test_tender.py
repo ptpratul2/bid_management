5d4c9f30ef55ae12e2d7d3fb679ca6982d143a8c
Himanshu Shivhare <himanshushivhare047@gmail.com>
2023-09-16 21:44:53 +0530

    file update

---
+++ bid_management/bid_management/doctype/tender/test_tender.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Himanshu Shivhare and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTender(FrappeTestCase):
+	pass
