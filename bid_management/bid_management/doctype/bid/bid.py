463edda8c192ca986d93cfd908ace3b3bb88e51e
erpnext <erpnext@vps.server.local>
2024-02-06 23:48:29 +0530

    doctype created

---
+++ bid_management/bid_management/doctype/bid/bid.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Himanshu Shivhare and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class Bid(NestedSet):
+	pass
---
+++ bid_management/bid_management/doctype/bid/test_bid.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Himanshu Shivhare and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBid(FrappeTestCase):
+	pass
