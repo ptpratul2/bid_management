542bd916b6ae76fc078d577072cabd050737a290
Pratul <ptpratul2@gmail.com>
2024-10-04 11:51:51 +0530

    extra doctype deleted

--- bid_management/bid_management/doctype/employee_weekoff/employee_weekoff.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024, Himanshu Shivhare and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Employee Weekoff", {
-// 	refresh(frm) {
-//         cur_frm.add_fetch('employee','holiday_list','holiday_list')
-
-// 	},
-// });
--- bid_management/bid_management/doctype/employee_weekoff/test_employee_weekoff.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Himanshu Shivhare and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestEmployeeWeekoff(FrappeTestCase):
-	pass
