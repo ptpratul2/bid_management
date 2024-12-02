a4b99cb02fa518756269250d04115967d3578f39
Himanshu Shivhare <himanshushivhare047@gmail.com>
2024-12-02 16:49:54 +0530

    Update globally_changes.js

--- bid_management/public/js/globally_changes.js
+++ bid_management/public/js/globally_changes.js
@@ -1,1 +1,25 @@
+$(window).on('hashchange', page_changed);
+$(window).on('load', page_changed);
 
+function page_changed(event) {
+    // Waiting for the page to load completely
+    frappe.after_ajax(function () {
+        var route = frappe.get_route();
+
+        if (route[0] === "Form") {
+            var doctype = route[1];
+
+            // Check if the event handler is already set
+            if (!frappe.ui.form.on[doctype]) {
+                frappe.ui.form.on(doctype, {
+                    refresh: function (frm) {
+                        console.log("doctype = " + frm.doctype);
+                        if (frappe.user_roles.includes("Administrator") || frappe.user_roles.includes("Desk User")) {
+                            cur_frm.page.menu.find('[data-label="' + __("Duplicate") + '"]').parent().parent().remove();
+                        }
+                    }
+                });
+            }
+        }
+    });
+}
